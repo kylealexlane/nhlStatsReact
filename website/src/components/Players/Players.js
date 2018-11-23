28074d85948a6089f62a590ed0c6a507cc2f4cc4
Jessie Won <jessiehywon@gmail.com>
2018-11-22 21:38:50 -0800

    add table component

--- website/src/components/Players/Players.js
+++ website/src/components/Players/Players.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled, { keyframes, withTheme } from "styled-components";
 import { Link } from "react-router-dom";
-import Typist from 'react-typist';
-import 'react-typist/dist/Typist.css';
-import { Table, Input, Button, Icon, Select } from 'antd';
-import compareByAlph from "../../functions/helpers"
-import theme from "../../styles/theme"
-
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import { Input, Button, Icon, Select } from "antd";
+import { Table } from "../Table";
+import compareByAlph from "../../functions/helpers";
+import theme from "../../styles/theme";
 
 const Wrapper = styled.div`
   ${props => props.theme.flex.flexColumnTopCenter};
@@ -43,89 +43,91 @@ const SelectDiv = styled.div`
   margin: 0;
   padding-left: 24px;
   flex: 0;
-
 `;
 
-const data = [{
-  key: '1',
-  first: 'Patrice',
-  last: 'Bergeron',
-  pos: 'C',
-  yearstart: '2017',
-  yearend: '2018',
-  shots: '211',
-  goals: '31',
-  kxgoals: '24.5',
-  sperc: '11.12',
-  kxsperc: '9.83',
-  kxss: '1.98',
-  shotquality: '1.32',
-}, {
-  key: '2',
-  first: 'Connor',
-  last: 'Mcdavid',
-  pos: 'C',
-  yearstart: '2016',
-  yearend: '2017',
-  shots: '322',
-  goals: '45',
-  kxgoals: '37.5',
-  sperc: '11.12',
-  kxsperc: '9.83',
-  kxss: '1.98',
-  shotquality: '1.32',
-},{
-  key: '3',
-  first: 'Mikko',
-  last: 'Rantenan',
-  pos: 'R',
-  yearstart: '2017',
-  yearend: '2018',
-  shots: '211',
-  goals: '31',
-  kxgoals: '24.5',
-  sperc: '11.12',
-  kxsperc: '9.83',
-  kxss: '1.98',
-  shotquality: '1.32',
-},{
-  key: '4',
-  first: 'Austin',
-  last: 'Matthews',
-  pos: 'C',
-  yearstart: '2016',
-  yearend: '2017',
-  shots: '211',
-  goals: '31',
-  kxgoals: '24.5',
-  sperc: '11.12',
-  kxsperc: '9.83',
-  kxss: '1.98',
-  shotquality: '1.32',
-},{
-  key: '5',
-  first: 'John',
-  last: 'Tavares',
-  pos: 'C',
-  yearstart: '2017',
-  yearend: '2018',
-  shots: '211',
-  goals: '31',
-  kxgoals: '24.5',
-  sperc: '11.12',
-  kxsperc: '9.83',
-  kxss: '1.98',
-  shotquality: '1.32',
-}];
-
-
+const data = [
+  {
+    key: "1",
+    first: "Patrice",
+    last: "Bergeron",
+    pos: "C",
+    yearstart: "2017",
+    yearend: "2018",
+    shots: "211",
+    goals: "31",
+    kxgoals: "24.5",
+    sperc: "11.12",
+    kxsperc: "9.83",
+    kxss: "1.98",
+    shotquality: "1.32"
+  },
+  {
+    key: "2",
+    first: "Connor",
+    last: "Mcdavid",
+    pos: "C",
+    yearstart: "2016",
+    yearend: "2017",
+    shots: "322",
+    goals: "45",
+    kxgoals: "37.5",
+    sperc: "11.12",
+    kxsperc: "9.83",
+    kxss: "1.98",
+    shotquality: "1.32"
+  },
+  {
+    key: "3",
+    first: "Mikko",
+    last: "Rantenan",
+    pos: "R",
+    yearstart: "2017",
+    yearend: "2018",
+    shots: "211",
+    goals: "31",
+    kxgoals: "24.5",
+    sperc: "11.12",
+    kxsperc: "9.83",
+    kxss: "1.98",
+    shotquality: "1.32"
+  },
+  {
+    key: "4",
+    first: "Austin",
+    last: "Matthews",
+    pos: "C",
+    yearstart: "2016",
+    yearend: "2017",
+    shots: "211",
+    goals: "31",
+    kxgoals: "24.5",
+    sperc: "11.12",
+    kxsperc: "9.83",
+    kxss: "1.98",
+    shotquality: "1.32"
+  },
+  {
+    key: "5",
+    first: "John",
+    last: "Tavares",
+    pos: "C",
+    yearstart: "2017",
+    yearend: "2018",
+    shots: "211",
+    goals: "31",
+    kxgoals: "24.5",
+    sperc: "11.12",
+    kxsperc: "9.83",
+    kxss: "1.98",
+    shotquality: "1.32"
+  }
+];
 
 function onChange(pagination, filters, sorter) {
-  console.log('params', pagination, filters, sorter);
+  console.log("params", pagination, filters, sorter);
 }
 
 const Option = Select.Option;
-
 
 function handleChangeYear(value) {
   console.log(`selected ${value}`);
@@ -139,7 +141,7 @@ class Players extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
+      searchText: ""
     };
   }
 
@@ -150,7 +152,7 @@ class Players extends React.Component {
 
   handleReset = clearFilters => () => {
     clearFilters();
-    this.setState({ searchText: '' });
+    this.setState({ searchText: "" });
   };
 
   componentDidMount() {
@@ -160,159 +162,231 @@ class Players extends React.Component {
   render() {
     const columns = [
       {
-        title: 'Last',
-        dataIndex: 'last',
-        defaultSortOrder: 'descend',
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        title: "Last",
+        dataIndex: "last",
+        defaultSortOrder: "descend",
+        filterDropdown: ({
+          setSelectedKeys,
+          selectedKeys,
+          confirm,
+          clearFilters
+        }) => (
           <div className="custom-filter-dropdown">
             <Input
-              ref={ele => this.searchInput = ele}
+              ref={ele => (this.searchInput = ele)}
               placeholder="Search name"
               value={selectedKeys[0]}
-              onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+              onChange={e =>
+                setSelectedKeys(e.target.value ? [e.target.value] : [])
+              }
               onPressEnter={this.handleSearch(selectedKeys, confirm)}
             />
-            <Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>Search</Button>
+            <Button
+              type="primary"
+              onClick={this.handleSearch(selectedKeys, confirm)}
+            >
+              Search
+            </Button>
             <Button onClick={this.handleReset(clearFilters)}>Reset</Button>
           </div>
         ),
-        filterIcon: filtered => <Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />,
-        onFilter: (value, record) => record.last.toLowerCase().includes(value.toLowerCase()),
-        onFilterDropdownVisibleChange: (visible) => {
+        filterIcon: filtered => (
+          <Icon
+            type="filter"
+            style={{ color: filtered ? "#108ee9" : "#aaa" }}
+          />
+        ),
+        onFilter: (value, record) =>
+          record.last.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
           if (visible) {
             setTimeout(() => {
               this.searchInput.focus();
             });
           }
         },
-        render: (text) => {
+        render: text => {
           const { searchText } = this.state;
           return searchText ? (
             <span>
-            {text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map((fragment, i) => (
-              fragment.toLowerCase() === searchText.toLowerCase()
-                ? <span key={i} className="highlight">{fragment}</span> : fragment // eslint-disable-line
-            ))}
-          </span>
-          ) : text;
+              {text
+                .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, "i"))
+                .map(
+                  (fragment, i) =>
+                    fragment.toLowerCase() === searchText.toLowerCase() ? (
+                      <span key={i} className="highlight">
+                        {fragment}
+                      </span>
+                    ) : (
+                      fragment
+                    ) // eslint-disable-line
+                )}
+            </span>
+          ) : (
+            text
+          );
         },
         sorter: (a, b) => compareByAlph(a.last, b.last),
-        fixed: 'left',
+        fixed: "left",
         width: 100
-      }, {
-      title: 'First',
-      dataIndex: 'first',
-      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-        <div className="custom-filter-dropdown">
-          <Input
-            ref={ele => this.searchInput = ele}
-            placeholder="Search name"
-            value={selectedKeys[0]}
-            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-            onPressEnter={this.handleSearch(selectedKeys, confirm)}
-          />
-          <Button type="primary" onClick={this.handleSearch(selectedKeys, confirm)}>Search</Button>
-          <Button onClick={this.handleReset(clearFilters)}>Reset</Button>
-        </div>
-      ),
-      filterIcon: filtered => <Icon type="filter" style={{ color: filtered ? '#108ee9' : '#aaa' }} />,
-      onFilter: (value, record) => record.first.toLowerCase().includes(value.toLowerCase()),
-      onFilterDropdownVisibleChange: (visible) => {
-        if (visible) {
-          setTimeout(() => {
-            this.searchInput.focus();
-          });
-        }
       },
-      render: (text) => {
-        const { searchText } = this.state;
-        return searchText ? (
-          <span>
-            {text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map((fragment, i) => (
-              fragment.toLowerCase() === searchText.toLowerCase()
-                ? <span key={i} className="highlight">{fragment}</span> : fragment // eslint-disable-line
-            ))}
-          </span>
-        ) : text;
-      },
-      sorter: (a, b) => compareByAlph(a.first, b.first),
-        width: 100
-    }, {
-      title: 'Pos',
-      dataIndex: 'pos',
-      filters: [{
-        text: 'C',
-        value: 'C',
-      }, {
-        text: 'R',
-        value: 'R',
-      }, {
-        text: 'L',
-        value: 'L',
-      }, {
-        text: 'D',
-        value: 'D',
-      }],
-      onFilter: (value, record) => record.pos.indexOf(value) === 0,
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => compareByAlph(a.pos, b.pos),
-        width: 100
-    },
-    //   {
-    //   title: 'Year Start',
-    //   dataIndex: 'yearstart',
-    //   defaultSortOrder: 'descend',
-    //   sorter: (a, b) => a.yearstart - b.yearstart,
-    // }, {
-    //   title: 'Year End',
-    //   dataIndex: 'yearend',
-    //   defaultSortOrder: 'descend',
-    //   sorter: (a, b) => a.yearend - b.yearend,
-    // },
       {
-      title: 'Shots',
-      dataIndex: 'shots',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.shots - b.shots,
-    }, {
-      title: 'Goals',
-      dataIndex: 'goals',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.goals - b.goals,
-    }, {
-      title: 'kxGoals',
-      dataIndex: 'kxgoals',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.kxgoals - b.kxgoals,
-    }, {
-      title: 'S%',
-      dataIndex: 'sperc',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.sperc - b.sperc,
-    }, {
-      title: 'kxS%',
-      dataIndex: 'kxsperc',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.kxsperc - b.kxsperc,
-    }, {
-      title: 'kxSS',
-      dataIndex: 'kxss',
-      defaultSortOrder: 'descend',
-      sorter: (a, b) => a.kxss - b.kxss,
-    }, {
-      title: 'Shot Quality',
-      dataIndex: 'shotquality',
-      sorter: (a, b) => a.shotquality - b.shotquality,
-    }];
+        title: "First",
+        dataIndex: "first",
+        filterDropdown: ({
+          setSelectedKeys,
+          selectedKeys,
+          confirm,
+          clearFilters
+        }) => (
+          <div className="custom-filter-dropdown">
+            <Input
+              ref={ele => (this.searchInput = ele)}
+              placeholder="Search name"
+              value={selectedKeys[0]}
+              onChange={e =>
+                setSelectedKeys(e.target.value ? [e.target.value] : [])
+              }
+              onPressEnter={this.handleSearch(selectedKeys, confirm)}
+            />
+            <Button
+              type="primary"
+              onClick={this.handleSearch(selectedKeys, confirm)}
+            >
+              Search
+            </Button>
+            <Button onClick={this.handleReset(clearFilters)}>Reset</Button>
+          </div>
+        ),
+        filterIcon: filtered => (
+          <Icon
+            type="filter"
+            style={{ color: filtered ? "#108ee9" : "#aaa" }}
+          />
+        ),
+        onFilter: (value, record) =>
+          record.first.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus();
+            });
+          }
+        },
+        render: text => {
+          const { searchText } = this.state;
+          return searchText ? (
+            <span>
+              {text
+                .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, "i"))
+                .map(
+                  (fragment, i) =>
+                    fragment.toLowerCase() === searchText.toLowerCase() ? (
+                      <span key={i} className="highlight">
+                        {fragment}
+                      </span>
+                    ) : (
+                      fragment
+                    ) // eslint-disable-line
+                )}
+            </span>
+          ) : (
+            text
+          );
+        },
+        sorter: (a, b) => compareByAlph(a.first, b.first),
+        width: 100
+      },
+      {
+        title: "Pos",
+        dataIndex: "pos",
+        filters: [
+          {
+            text: "C",
+            value: "C"
+          },
+          {
+            text: "R",
+            value: "R"
+          },
+          {
+            text: "L",
+            value: "L"
+          },
+          {
+            text: "D",
+            value: "D"
+          }
+        ],
+        onFilter: (value, record) => record.pos.indexOf(value) === 0,
+        defaultSortOrder: "descend",
+        sorter: (a, b) => compareByAlph(a.pos, b.pos),
+        width: 100
+      },
+      //   {
+      //   title: 'Year Start',
+      //   dataIndex: 'yearstart',
+      //   defaultSortOrder: 'descend',
+      //   sorter: (a, b) => a.yearstart - b.yearstart,
+      // }, {
+      //   title: 'Year End',
+      //   dataIndex: 'yearend',
+      //   defaultSortOrder: 'descend',
+      //   sorter: (a, b) => a.yearend - b.yearend,
+      // },
+      {
+        title: "Shots",
+        dataIndex: "shots",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.shots - b.shots
+      },
+      {
+        title: "Goals",
+        dataIndex: "goals",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.goals - b.goals
+      },
+      {
+        title: "kxGoals",
+        dataIndex: "kxgoals",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.kxgoals - b.kxgoals
+      },
+      {
+        title: "S%",
+        dataIndex: "sperc",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.sperc - b.sperc
+      },
+      {
+        title: "kxS%",
+        dataIndex: "kxsperc",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.kxsperc - b.kxsperc
+      },
+      {
+        title: "kxSS",
+        dataIndex: "kxss",
+        defaultSortOrder: "descend",
+        sorter: (a, b) => a.kxss - b.kxss
+      },
+      {
+        title: "Shot Quality",
+        dataIndex: "shotquality",
+        sorter: (a, b) => a.shotquality - b.shotquality
+      }
+    ];
     return (
       <React.Fragment>
         <Wrapper>
           <TopDiv>
-            <StyledHeader>
-              Shooters
-            </StyledHeader>
+            <StyledHeader>Shooters</StyledHeader>
             <SelectDiv>
-              <Select defaultValue="2018" style={{ width: 160 }} onChange={handleChangeYear}>
+              <Select
+                defaultValue="2018"
+                style={{ width: 160 }}
+                onChange={handleChangeYear}
+              >
                 <Option value="2011">2011-2012</Option>
                 <Option value="2012">2012-2013</Option>
                 <Option value="2013">2013-2014</Option>
@@ -324,19 +398,26 @@ class Players extends React.Component {
               </Select>
             </SelectDiv>
             <SelectDiv>
-              <Select defaultValue="0" style={{ width: 160 }} onChange={handleChangePlayoff}>
+              <Select
+                defaultValue="0"
+                style={{ width: 160 }}
+                onChange={handleChangePlayoff}
+              >
                 <Option value="0">Regular Season</Option>
                 <Option value="1">Playoffs</Option>
               </Select>
             </SelectDiv>
           </TopDiv>
-          <Table columns={columns} dataSource={data} onChange={onChange} scroll={{ x: 1300 }}/>
+          <Table
+            columns={columns}
+            dataSource={data}
+            onChange={onChange}
+            scroll={{ x: 1300 }}
+          />
         </Wrapper>
       </React.Fragment>
     );
   }
 }
-
-
 
 export default withTheme(Players);
